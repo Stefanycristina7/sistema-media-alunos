35d707c998052263a5eb2adf980ee049d25d35cc
e <stefanycristinas009@gmail.com>
2026-03-03 00:12:37 -0300

    feat: implementa cadastro de alunos com cálculo de média

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String nome;
+        double nota1, nota2, nota3,media;
 
+        System.out.print("Informe a quantidade de alunos: ");
+        int totalAlunos = entrada.nextInt();
+        entrada.nextLine();
+
+        for(int i = 0; i < totalAlunos;i++){
+            System.out.println("Digite o nome completo do aluno: ");
+            nome = entrada.nextLine();
+            System.out.println("Digite nota 1: ");
+            nota1 = entrada.nextDouble();
+            System.out.println("Digite nota 2: ");
+            nota2 = entrada.nextDouble();
+            System.out.println("Digite nota 3: ");
+            nota3 = entrada.nextDouble();
+
+            media = (nota1 + nota2 + nota3)/3;
+            entrada.nextLine();
+
+
+        }
     }
 }
